b82d5b0a07e875873d617b45edb13acf9616cb9c
bripkens.dev <bripkens.dev@d6304f67-dacb-79bb-6816-61a379c1875f>
2010-09-13 11:57:45 +0000

    Added persistence unit, we can now try a bunch of things

--- branches/prototype/DecisionManagement/src/main/java/nl/rug/search/odr/SimpleBean.java
+++ branches/prototype/DecisionManagement/src/main/java/nl/rug/search/odr/SimpleBean.java
@@ -2,6 +2,9 @@
 package nl.rug.search.odr;
 
 import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import nl.rug.search.odr.entities.SystemUser;
 
 /**
  *
@@ -13,7 +16,14 @@ public class SimpleBean implements SimpleBeanLocal {
     // Add business logic below. (Right-click in editor and choose
     // "Insert Code > Add Business Method")
 
+    @PersistenceContext
+    private EntityManager entityManager;
+
     public String getText() {
+        SystemUser u = new SystemUser();
+        u.setName("Peter");
+        entityManager.persist(u);
+
         return "42";
     }
  
---
+++ branches/prototype/DecisionManagement/src/main/java/nl/rug/search/odr/entities/SystemUser.java
@@ -0,0 +1,65 @@
+
+package nl.rug.search.odr.entities;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author Ben Ripkens <bripkens.dev@gmail.com>
+ */
+@Entity
+public class SystemUser implements Serializable {
+    private static final long serialVersionUID = 1L;
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof SystemUser)) {
+            return false;
+        }
+        SystemUser other = (SystemUser) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "nl.rug.search.odr.entities.User[id=" + id + "]";
+    }
+
+}
