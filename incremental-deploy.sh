acec0150bc2d4a604b11e8ce96b9660bb3401f6e
Ben Ripkens <bripkens.dev@gmail.com>
2010-09-14 14:09:36 +0000

    Added an error message property file and started with create project

--- incremental-deploy.sh
+++ incremental-deploy.sh
@@ -4,6 +4,9 @@ while true
 do
 
 rsync -avz "./WebInterface/src/main/webapp/" "./OpenDecisionRepository/target/gfdeploy/WebInterface-0.1_war"
+rsync -avz "./WebInterface/src/main/resources/nl/rug/search/odr/localization/" "./OpenDecisionRepository/target/gfdeploy/WebInterface-0.1_war/WEB-INF/classes/nl/rug/search/odr/localization"
 
-sleep 5
+# use either enter to redeploy or do it every 5 seconds
+read line
+# sleep 5
 done
