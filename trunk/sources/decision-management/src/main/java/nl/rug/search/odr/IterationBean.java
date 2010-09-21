c75a858a5d978e8026aa8ac5fd12b0655db69bca
stefan.arians <stefan.arians@d6304f67-dacb-79bb-6816-61a379c1875f>
2010-09-21 14:00:36 +0000

    iterationbeans

---
+++ trunk/sources/decision-management/src/main/java/nl/rug/search/odr/IterationBean.java
@@ -0,0 +1,23 @@
+
+package nl.rug.search.odr;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import nl.rug.search.odr.entities.Iteration;
+
+/**
+ *
+ * @author Stefan
+ */
+@Stateless
+public class IterationBean implements IterationLocal {
+    @PersistenceContext
+    private EntityManager entityManager;
+
+
+    @Override
+    public void addIteration(Iteration i) {
+        entityManager.persist(i);
+    }
+}
