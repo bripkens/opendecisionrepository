46ce6727c3dac1be3a026e8ea399c53a5e0d45bb
stefan.arians <sarians@users.noreply.github.com>
2010-09-21 14:00:36 +0000

    iterationbeans

---
+++ sources/decision-management/src/main/java/nl/rug/search/odr/IterationLocal.java
@@ -0,0 +1,14 @@
+
+package nl.rug.search.odr;
+
+import nl.rug.search.odr.entities.Iteration;
+
+/**
+ *
+ * @author Stefan
+ */
+public interface IterationLocal {
+    
+    void addIteration(Iteration i);
+    
+}
