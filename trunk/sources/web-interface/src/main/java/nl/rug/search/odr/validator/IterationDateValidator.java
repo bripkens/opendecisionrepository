77a7d16aaab0afc5f92f0bb61be9df39c7497c7a
bripkens.dev <bripkens.dev@d6304f67-dacb-79bb-6816-61a379c1875f>
2010-09-21 14:00:03 +0000

    Changed some imports

--- trunk/sources/web-interface/src/main/java/nl/rug/search/odr/validator/IterationDateValidator.java
+++ trunk/sources/web-interface/src/main/java/nl/rug/search/odr/validator/IterationDateValidator.java
@@ -2,15 +2,10 @@ package nl.rug.search.odr.validator;
 
 import com.sun.faces.util.MessageFactory;
 import java.util.Date;
-import javax.ejb.EJB;
-import javax.faces.FacesException;
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
 import javax.faces.validator.Validator;
 import javax.faces.validator.ValidatorException;
-import nl.rug.search.odr.EjbUtil;
-import nl.rug.search.odr.ProjectLocal;
-import nl.rug.search.odr.StringValidator;
 
 /**
  *
