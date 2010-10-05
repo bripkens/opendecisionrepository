579f9d51e53ce9fb892a953ea5300d65c758339d
stefan.arians <sarians@users.noreply.github.com>
2010-10-05 09:09:11 +0000

    project and iteration entity from new domain model!

--- sources/entities/src/main/java/nl/rug/search/odr/entities/Iteration.java
+++ sources/entities/src/main/java/nl/rug/search/odr/entities/Iteration.java
@@ -4,16 +4,13 @@
  */
 package nl.rug.search.odr.entities;
 
-import java.util.ArrayList;
-import java.util.Collection;
+import java.util.Comparator;
 import java.util.Date;
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToMany;
 import javax.persistence.Temporal;
 import nl.rug.search.odr.BusinessException;
 import nl.rug.search.odr.StringValidator;
@@ -23,55 +20,34 @@ import nl.rug.search.odr.StringValidator;
  * @author Stefan
  */
 @Entity
-public class Iteration extends BaseEntity<Iteration>{
+public class Iteration extends BaseEntity<Iteration> {
 
     private static final long serialVersionUID = 1L;
+//
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-    @Column
+//
+    @Column(length = 30, nullable = false, unique = true)
     private String name;
-    @Column
+//
+    @Column(length = 500)
     private String description;
-    @Column
+//
+    @Column(nullable = false)
     @Temporal(javax.persistence.TemporalType.DATE)
     private Date startDate;
+//
     @Column
     @Temporal(javax.persistence.TemporalType.DATE)
     private Date endDate;
-    @ManyToMany(cascade = CascadeType.ALL)
-    private Collection<Version> versions;
 
-    public Iteration() {
-        versions = new ArrayList<Version>();
-    }
-
-    public Collection<Version> getVersions() {
-        return versions;
-    }
-
-    public void setVersions(Collection<Version> versions) {
-        this.versions = versions;
-    }
-
-    public void removeVersion(Version version) {
-        if (version == null) {
-            throw new BusinessException("remove version is null.");
-        }
-        versions.remove(version);
-    }
-
-    public void addVersion(Version version) {
-        if (version == null) {
-            throw new BusinessException("Version is null.");
-        }
-        versions.add(version);
-    }
-
+    @Override
     public Long getId() {
         return id;
     }
 
+    @Override
     public void setId(Long id) {
         if (id == null) {
             throw new BusinessException("Id is null.");
@@ -79,8 +55,16 @@ public class Iteration extends BaseEntity<Iteration>{
         this.id = id;
     }
 
-    public String getDescription() {
+    public String getName() {
+        return name;
+    }
 
+    public void setName(String name) {
+        StringValidator.isValid(name);
+        this.name = name;
+    }
+
+    public String getDescription() {
         return description;
     }
 
@@ -102,15 +86,6 @@ public class Iteration extends BaseEntity<Iteration>{
         this.endDate = endDate;
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        StringValidator.isValid(name);
-        this.name = name;
-    }
-
     public Date getStartDate() {
         return startDate;
     }
@@ -118,27 +93,54 @@ public class Iteration extends BaseEntity<Iteration>{
     public void setStartDate(Date startDate) {
         if (startDate == null) {
             throw new BusinessException("Start date is null.");
-        }
-        if (endDate != null) {
-            if (startDate.getTime() < endDate.getTime()) {
-                this.startDate = startDate;
-            } else {
-                throw new BusinessException("the startDate has to be before the endDate");
-            }
+        } else if (endDate != null && startDate.getTime() > endDate.getTime()) {
+            throw new BusinessException("the startDate has to be before the endDate");
         }
         this.startDate = startDate;
-
     }
 
     public boolean isPersistable() {
-        if (name == null) {
+        if (name == null && startDate == null) {
             return false;
         }
         return true;
     }
 
+    public long getDurationInDays() {
+        if (startDate == null) {
+            throw new BusinessException("can calculate duration without a startDate");
+        }
+        long endMilliseconds = 0;
+        if (endDate != null) {
+            endMilliseconds = endDate.getTime();
+        } else {
+            endMilliseconds = new Date().getTime();
+        }
+
+        long duration = 0;
+        duration = endMilliseconds - startDate.getTime();
+        duration /= 24 * 60 * 60 * 1000;
+        return duration;
+    }
+
     @Override
     protected Object[] getCompareData() {
         return new Object[]{name, description, startDate, endDate};
+
+    }
+
+    public static class IterationEndDateComparator implements Comparator<Iteration> {
+
+        @Override
+        public int compare(Iteration o1, Iteration o2) {
+            if (o1.endDate == null && o2.endDate == null) {
+                return 0;
+            } else if (o1.endDate == null) {
+                return -1;
+            } else if (o2.endDate == null) {
+                return 1;
+            }
+            return o1.endDate.compareTo(o2.endDate);
+        }
     }
 }
