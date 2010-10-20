ae07280ebff9b8095c3b5d186bfc7e0ae86896f5
bripkens.dev <bripkens.dev@d6304f67-dacb-79bb-6816-61a379c1875f>
2010-10-20 11:43:20 +0000

    Fixed an error that didn't allowed the container to start

--- trunk/sources/entities/src/main/java/nl/rug/search/odr/entities/StakeholderRole.java
+++ trunk/sources/entities/src/main/java/nl/rug/search/odr/entities/StakeholderRole.java
@@ -14,52 +14,78 @@ import nl.rug.search.odr.StringValidator;
  */
 @Entity
 public class StakeholderRole extends BaseEntity<StakeholderRole> {
+
     @Id
-    @GeneratedValue(strategy=GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     private static final long serialVersionUID = 1L;
-   
+
     @Column
     private String name;
+
     @Column
     private boolean common;
 
+
+
+
     @Override
-    public Long getId(){
+    public Long getId() {
         return id;
     }
 
+
+
+
     @Override
-    public void setId(Long id){
-        if(id == null){
+    public void setId(Long id) {
+        if (id == null) {
             throw new BusinessException("Please provide an id");
         }
         this.id = id;
     }
 
+
+
+
     public String getName() {
         return name;
     }
 
+
+
+
     public void setName(String name) {
         StringValidator.isValid(name);
-        
+
         this.name = name;
     }
+
+
+
 
     public boolean isCommon() {
         return common;
     }
 
+
+
+
     public void setCommon(boolean common) {
         this.common = common;
     }
 
+
+
+
     @Override
     protected Object[] getCompareData() {
-       return new Object[]{name, common};
+        return new Object[]{name, common};
     }
+
+
+
 
     @Override
     public boolean isPersistable() {
