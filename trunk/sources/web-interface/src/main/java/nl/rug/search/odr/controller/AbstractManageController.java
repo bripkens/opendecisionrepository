83064b7e71bd0bd6c24adc39c6f7c8e7e92120e0
bripkens.dev <bripkens.dev@d6304f67-dacb-79bb-6816-61a379c1875f>
2010-11-01 15:13:33 +0000

    Refactored the CSS

--- trunk/sources/web-interface/src/main/java/nl/rug/search/odr/controller/AbstractManageController.java
+++ trunk/sources/web-interface/src/main/java/nl/rug/search/odr/controller/AbstractManageController.java
@@ -3,7 +3,6 @@ package nl.rug.search.odr.controller;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.ejb.EJB;
-import nl.rug.search.odr.RequestParameterAnalyser;
 import nl.rug.search.odr.Mode;
 import javax.faces.context.FacesContext;
 import javax.servlet.http.HttpServletRequest;
@@ -143,5 +142,152 @@ public abstract class AbstractManageController extends AbstractController implem
 
     private boolean isIdSet() {
         return getPreviousEntityId() != null;
+    }
+
+
+    private static class RequestParameterAnalyser implements RequestParameter {
+
+        public static final long ID_NOT_SET = -1;
+
+        private final HttpServletRequest request;
+
+        private final boolean previousEntityExisting;
+
+        private boolean create, update, delete, confirm;
+
+        private long id;
+
+        private Mode mode;
+
+
+
+
+        public RequestParameterAnalyser(HttpServletRequest request, boolean previousEntityExisting) {
+            this.request = request;
+            this.previousEntityExisting = previousEntityExisting;
+
+            readParameters();
+
+            this.mode = determineRequestMode();
+        }
+
+
+
+
+        private void readParameters() {
+            create = request.getParameter(CREATE) != null;
+            update = request.getParameter(UPDATE) != null;
+            delete = request.getParameter(DELETE) != null;
+            confirm = request.getParameter(CONFIRM) != null;
+
+            String idParam = request.getParameter(ID);
+
+            try {
+                id = Long.parseLong(idParam);
+            } catch (NumberFormatException ex) {
+                id = ID_NOT_SET;
+            }
+
+            if (id < 0) {
+                id = ID_NOT_SET;
+            }
+        }
+
+
+
+
+        private Mode determineRequestMode() {
+            int paramCounter = 0;
+            paramCounter += (create ? 1 : 0);
+            paramCounter += (update ? 1 : 0);
+            paramCounter += (delete ? 1 : 0);
+
+            if (paramCounter > 1) {
+                return Mode.ILLEGAL;
+            }
+
+            if (isDeleteSet() && isIdSet()) {
+
+                if (isConfirmSet()) {
+                    return Mode.DELETE_CONFIRMED;
+                }
+
+                return Mode.DELETE;
+
+            } else if (isCreateSet()) {
+                return Mode.CREATE;
+            } else if (isUpdateSet() && isIdSet()) {
+                return Mode.UPDATE;
+            } else if (isPreviousEntityExisting()) {
+                return Mode.STAY_IN_CURRENT;
+            }
+
+            return Mode.ILLEGAL;
+        }
+
+        // <editor-fold defaultstate="collapsed" desc="Getter">
+
+
+
+        private boolean isPreviousEntityExisting() {
+            return previousEntityExisting;
+        }
+
+
+
+
+        public boolean isConfirmSet() {
+            return confirm;
+        }
+
+
+
+
+        public boolean isCreateSet() {
+            return create;
+        }
+
+
+
+
+        public boolean isDeleteSet() {
+            return delete;
+        }
+
+
+
+
+        public long getId() {
+            return id;
+        }
+
+
+
+
+        public boolean isUpdateSet() {
+            return update;
+        }
+
+
+
+
+        public boolean isIdSet() {
+            return id != ID_NOT_SET;
+        }
+
+
+
+
+        public Mode getMode() {
+            return mode;
+        }
+
+
+
+
+        public boolean isValidRequest() {
+            return getMode() != null && getMode() != Mode.ILLEGAL;
+        }
+        // </editor-fold>
     }
 }
