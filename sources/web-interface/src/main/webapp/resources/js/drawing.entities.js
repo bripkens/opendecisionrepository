e71457381d7678a3a5f845bfccc7ad189bf36821
Ben Ripkens <bripkens.dev@gmail.com>
2010-11-09 21:27:46 +0000

    Some good progress on the drawing (generation of views) javascript. Finished the foundation for all entities. Entity Rectangle (which can be used to represent decisions is finished

--- sources/web-interface/src/main/webapp/resources/js/drawing.entities.js
+++ sources/web-interface/src/main/webapp/resources/js/drawing.entities.js
@@ -24,7 +24,7 @@ odr.init(function() {
  *                              Registry class
  */
 odr.Registry = function() {
-    this._items = new Object();
+    this._items = {};
 }
 odr.Registry.prototype = {
     _items : null,
@@ -50,43 +50,74 @@ odr.Registry.prototype = {
 
 odr.Callback = function() {
     this._listeners = {
-        draw : new Array(),
-        redraw : new Array(),
-        remove : new Array(),
-        dragStart : new Array(),
-        dragging : new Array(),
-        dragEnd : new Array()
+        draw : {},
+        redraw : {},
+        remove : {},
+        dragStart : {},
+        dragging : {},
+        dragEnd : {}
     };
 }
+
+odr.Callback.types = {
+    draw : "draw",
+    redraw : "redraw",
+    remove : "remove",
+    dragStart : "dragStart",
+    dragging : "dragging",
+    dragEnd : "dragEnd"
+}
+
 odr.Callback.prototype = {
-    listeners : null,
-    draw : function(listener) {
-        this.handle(listener, this._listeners.draw);
+    _listeners : null,
+    draw : function(listener, identifier) {
+        this.handle(listener, identifier, this._listeners.draw);
     },
-    redraw : function(listener) {
-        this.handle(listener, this._listeners.redraw);
+    redraw : function(listener, identifier) {
+        this.handle(listener, identifier, this._listeners.redraw);
     },
-    remove : function(listener) {
-        this.handle(listener, this._listeners.remove);
+    remove : function(listener, identifier) {
+        this.handle(listener, identifier, this._listeners.remove);
     },
-    dragStart : function(listener) {
-        this.handle(listener, this._listeners.dragStart);
+    dragStart : function(listener, identifier) {
+        this.handle(listener, identifier, this._listeners.dragStart);
     },
-    dragging : function(listener) {
-        this.handle(listener, this._listeners.dragging);
+    dragging : function(listener, identifier) {
+        this.handle(listener, identifier, this._listeners.dragging);
     },
-    dragEnd : function(listener) {
-        this.handle(listener, this._listeners.dragEnd);
+    dragEnd : function(listener, identifier) {
+        this.handle(listener, identifier, this._listeners.dragEnd);
     },
-    handle : function(listener, collection) {
-        if (listener) {
-            collection[collection.length] = listener;
+    handle : function(listener, identifier, collection) {
+        if (listener && identifier) {
+            collection[identifier] = listener;
             return;
         }
 
         for(var i in collection) {
             collection[i]();
         }
+    },
+    unbind : function(type, identifier) {
+        var collection = undefined;
+
+        if (type == odr.Callback.types.draw) {
+            collection = this._listeners.draw;
+        } else if (type == odr.Callback.types.redraw) {
+            collection = this._listeners.redraw;
+        } else if (type == odr.Callback.types.remove) {
+            collection = this._listeners.remove;
+        } else if (type == odr.Callback.types.dragStart) {
+            collection = this._listeners.dragStart;
+        } else if (type == odr.Callback.types.dragging) {
+            collection = this._listeners.dragging;
+        } else if (type == odr.Callback.types.dragEnd) {
+            collection = this._listeners.dragEnd;
+        } else {
+            throw("Listener type is not defined");
+        }
+
+        delete collection[identifier];
     }
 }
 
@@ -145,6 +176,9 @@ odr.DrawableItem.prototype = {
     },
     id : function() {
         return this._id;
+    },
+    extendedId : function() {
+        throw("Abstract method");
     }
 }
 
@@ -256,7 +290,7 @@ extend(odr.Endpoint, odr.Shape);
 
 /*
  * ###########################################################################
- *                              Node class
+ *                              Rectangle class
  */
 odr.Rectangle = function() {
     odr.Endpoint.call(this);
@@ -282,14 +316,220 @@ odr.Rectangle.prototype = {
         }
     },
     paint : function() {
+        j("#" + this.extendedId()).remove();
+
+        var parent = this.parent();
+
+        if (!parent) {
+            parent = j("#" + odr.rectangleSettings.group);
+        }
+
+        odr._svg.rect(parent, this.x(), this.y(), this.width(),
+            this.height(), odr.rectangleSettings.rx, odr.rectangleSettings.ry, {
+                "class" : odr.rectangleSettings["class"],
+                "id" : this.extendedId()
+            });
+
+        odr.assertContainerSize(this.extendedId());
+
         odr.Rectangle.superClass.draw.call(this);
     },
     repaint : function() {
+        var element = j("#" + this.extendedId());
+        element.attr("width", this.width());
+        element.attr("height", this.height());
+        element.attr("x", this.x());
+        element.attr("y", this.y());
+
+        odr.assertContainerSize(this.extendedId());
+
         odr.Rectangle.superClass.redraw.call(this);
     },
     dispose : function() {
         odr.Rectangle.superClass.remove.call(this);
+    },
+    extendedId : function() {
+        return odr.rectangleSettings.idPrefix + this.id();
     }
 }
 
 extend(odr.Rectangle, odr.Endpoint);
+
+
+
+
+
+
+
+
+
+/*
+ * ###########################################################################
+ *                              Handle class
+ */
+odr.Handle = function() {
+    odr.Shape.call(this);
+}
+
+odr.Handle.prototype = {
+    center : function() {
+        return {
+            x : this.x(),
+            y : this.y()
+        };
+    },
+    topLeft : function() {
+        return {
+            x : this.x() - (this.width() / 2),
+            y : this.y() - (this.height() / 2)
+        }
+    },
+    bottomRight : function() {
+        return {
+            x : this.x() + (this.width() / 2),
+            y : this.y() + (this.height() / 2)
+        }
+    },
+    paint : function() {
+        odr.Handle.superClass.draw.call(this);
+    },
+    repaint : function() {
+        odr.Handle.superClass.redraw.call(this);
+    },
+    dispose : function() {
+        odr.Handle.superClass.remove.call(this);
+    },
+    extendedId : function() {
+        return odr.handleSettings.idPrefix + this.id();
+    }
+}
+
+extend(odr.Handle, odr.Shape);
+
+
+
+
+
+
+
+
+/*
+ * ###########################################################################
+ *                              Line class
+ */
+odr.Line = function() {
+    odr.DrawableItem.call(this);
+    this._start = null;
+    this._end = null;
+}
+
+odr.Line.prototype = {
+    _start : null,
+    _end : null,
+    start : function(start) {
+        if (start) {
+            if (this._start) {
+                this._start.unbind(odr.Callback.types.redraw, this.extendedId());
+            }
+
+            this._start = start;
+            this._start.redraw(this.repaint.createDelegate(this), this.extendedId());
+            
+            return this;
+        }
+
+        return this._start;
+    },
+    end : function(end) {
+        if (end) {
+            if (this._end) {
+                this._end.unbind(odr.Callback.types.redraw, this.extendedId());
+            }
+
+            this._end = end;
+
+            this._end.redraw(this.repaint.createDelegate(this), this.extendedId());
+
+            return this;
+        }
+
+        return this._end;
+    },
+    paint : function() {
+        odr.Line.superClass.draw.call(this);
+    },
+    repaint : function() {
+        odr.Line.superClass.redraw.call(this);
+    },
+    dispose : function() {
+        odr.Line.superClass.remove.call(this);
+    },
+    extendedId : function() {
+        return odr.lineSettings.idPrefix + this.id();
+    }
+}
+
+extend(odr.Line, odr.DrawableItem);
+
+
+
+
+
+
+
+
+
+/*
+ * ###########################################################################
+ *                              Line class
+ */
+odr.Association = function() {
+    odr.DrawableItem.call(this);
+    this._source = null;
+    this._target = null;
+    this._label = null;
+}
+
+odr.Association.prototype = {
+    _source : null,
+    _target : null,
+    _label : null,
+    source : function(source) {
+        if (source) {
+            this._source = source;
+            return this;
+        }
+
+        return this._source;
+    },
+    target : function(target) {
+        if (target) {
+            this._target = target;
+            return this;
+        }
+
+        return this._target;
+    },
+    label : function(label) {
+        if (label) {
+            this._label = label;
+            return this;
+        }
+
+        return this._label;
+    },
+    paint : function() {
+        odr.Association.superClass.draw.call(this);
+    },
+    repaint : function() {
+        odr.Association.superClass.redraw.call(this);
+    },
+    dispose : function() {
+        odr.Association.superClass.remove.call(this);
+    },
+    extendedId : function() {
+        return odr.associationSettings.idPrefix + this.id();
+    }
+}
+
+extend(odr.Association, odr.DrawableItem);
