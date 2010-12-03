636b52f4a86b2865dd558a1f9c20eceabab16b2b
Ben Ripkens <bripkens.dev@gmail.com>
2010-12-03 11:41:15 +0000

    Refactored entities/src/main/java/nl/rug/search/odr/viewpoint/chronological/InitChronologicalView.java. Starting to test the class

---
+++ sources/entities/src/main/java/nl/rug/search/odr/viewpoint/chronological/ViewBlock.java
@@ -0,0 +1,62 @@
+package nl.rug.search.odr.viewpoint.chronological;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import nl.rug.search.odr.entities.Iteration;
+import nl.rug.search.odr.entities.Version;
+
+class ViewBlock implements Comparable<ViewBlock> {
+
+    private Iteration iteration;
+
+    private Map<List<Long>, List<Version>> versionsForStakeholderGroups;
+
+
+
+
+    public ViewBlock(Iteration it) {
+        this.iteration = it;
+        this.versionsForStakeholderGroups = new HashMap<List<Long>, List<Version>>();
+    }
+
+
+
+
+    public void addVersion(List<Long> group, Version v) {
+        List<Version> versions = versionsForStakeholderGroups.get(group);
+        if (versions == null) {
+            versions = new ArrayList<Version>();
+            versionsForStakeholderGroups.put(group, versions);
+        }
+        versions.add(v);
+    }
+
+
+
+
+    public List<List<Version>> getOrderedVersions() {
+        List<List<Version>> allVersionsInGroups = new ArrayList<List<Version>>(versionsForStakeholderGroups.size());
+        for (Entry<List<Long>, List<Version>> entry : versionsForStakeholderGroups.entrySet()) {
+            Collections.sort(entry.getValue(), new Version.DecidedWhenComparator());
+            allVersionsInGroups.add(entry.getValue());
+        }
+        return allVersionsInGroups;
+    }
+
+
+
+
+    public Iteration getIteration() {
+        return iteration;
+    }
+
+
+    @Override
+    public int compareTo(ViewBlock o) {
+        return iteration.getStartDate().compareTo(o.iteration.getStartDate());
+    }
+}
