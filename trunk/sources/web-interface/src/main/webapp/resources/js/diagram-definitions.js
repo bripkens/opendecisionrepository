ff29c20740c80f81d955a4785efe3579a27f4267
bripkens.dev <bripkens.dev@d6304f67-dacb-79bb-6816-61a379c1875f>
2010-12-07 23:49:37 +0000

    Further experiments with svgweb and drawing nodes using standard html components + jQuery ui

--- trunk/sources/web-interface/src/main/webapp/resources/js/diagram-definitions.js
+++ trunk/sources/web-interface/src/main/webapp/resources/js/diagram-definitions.js
@@ -1,2 +1,72 @@
+/**
+ * @fileOverview
+ *
+ * This file contains definitions, settings and variable declarations
+ *
+ * @author Ben Ripkens <bripkens.dev@gmail.com>
+ */
+
+/**
+ * @namespace The namespace for the whole diagram JavaScript.
+ */
+var odr = odr || {};
 
 
+
+
+
+/*
+ * ###############################################################################################################
+ *                                              Settings
+ */
+
+/**
+ * @namespace
+ * ODR settings
+ */
+odr.settings = {
+    menu : {
+        top : {
+            "class" : "topMenu",
+            expandedHeight : function() {
+                return "-15px";
+            },
+            collapsedHeight : function(element) {
+                return (element.height() + 11) * -1;
+            },
+            animationDuration : 500
+        }
+    },
+    dragging : {
+        jQueryUiSettings : {
+            grid : [10, 10],
+            snap : true,
+            scroll: true,
+            stack : ".node"
+        }
+    },
+    resizing : {
+        jQueryUiSettings : {
+            grid : 10
+        }
+    }
+};
+
+
+
+
+
+
+
+/*
+ * ###############################################################################################################
+ *                                              Variable declarations
+ */
+
+/**
+ * @private
+ */
+odr.vars = {
+    bootstrapFunctions : [],
+    readyFunctions : []
+}
