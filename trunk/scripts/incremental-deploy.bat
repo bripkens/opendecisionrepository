cc402681ab44411d4330360da6ada5cef029525c
bripkens.dev <bripkens.dev@d6304f67-dacb-79bb-6816-61a379c1875f>
2011-04-25 06:21:27 +0000

    Moved the scripts into their own directory /trunk/scripts. Removed some println statements from the url rewrite filter.

---
+++ trunk/scripts/incremental-deploy.bat
@@ -0,0 +1,6 @@
+:x
+rsync -avz "../sources/web-interface/src/main/webapp/" "../sources/startup/target/gfdeploy/nl.rug.search_startup_ear_0.1/web-interface-0.1_war"
+
+pause
+
+goto x
--- trunk/sources/watch-css-changes.sh
+++
@@ -1,1 +0,0 @@
-sass --watch web-interface/src/main/webapp/resources/css/common.scss:web-interface/src/main/webapp/resources/css/common.css
