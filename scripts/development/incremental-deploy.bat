579fdc793d0d3bbe76fca0c7955acf6934904d86
Ben Ripkens <bripkens.dev@gmail.com>
2011-11-21 09:14:24 +0000

    [maven-release-plugin]  copy for tag web-service-client-0.2.2

--- scripts/development/incremental-deploy.bat
+++
@@ -1,6 +0,0 @@
-:x
-rsync -avz "../sources/web-interface/src/main/webapp/" "../sources/startup/target/gfdeploy/nl.rug.search_startup_ear_0.1/web-interface-0.1_war"
-
-pause
-
-goto x
--- scripts/development/watch-css-changes.bat
+++
@@ -1,1 +0,0 @@
-sass --watch ../sources/web-interface/src/main/webapp/resources/css/common.scss:../sources/web-interface/src/main/webapp/resources/css/common.css
